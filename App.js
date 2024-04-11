b9f1f5426da417aba53d3f98e34f24e0fb9f4632
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-04-11 15:13:47 -0300

    feat: :art: Create Styles.js

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Title from './src/components/Title';
-import Main from './src/components/Main';
+import Form from './src/components/Form'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Title/>
-      <Main/>
+      <Form/>
     </View>
   );
 }
@@ -16,8 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#e0e5e5',
+    paddingTop: 80,
   },
 });
