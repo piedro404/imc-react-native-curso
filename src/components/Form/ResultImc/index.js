54c4f30a645b4094b180f18f0b6b03217073fe01
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-04-13 11:16:27 -0300

    feat: :seedling: Custom Result

--- src/components/Form/ResultImc/index.js
+++ src/components/Form/ResultImc/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
+import styles from "./style";
 
 export default function ResultImc(props) {
     return(
-        <View>
-            <Text>{props.messageResultImc}</Text>
-            <Text>{props.resultImc}</Text>
+        <View style={styles.resultImc}>
+            <Text style={styles.info}>{props.messageResultImc}</Text>
+            <Text style={styles.numberImc}>{props.resultImc}</Text>
         </View>
     );
 }
---
+++ src/components/Form/ResultImc/style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    resultImc: {
+        flex: 1,
+        marginTop: 15,
+        paddingTop: 60,
+        borderRadius: 50,
+        alignItems: "center",
+        width: "100%",
+    },
+    info: {
+        fontSize: 18,
+        color: "#ff0043",
+        fontWeight: "bold",
+    },
+    numberImc: {
+        fontSize: 48,
+        color: "#ff0043",
+        fontWeight: "bold",
+    }
+});
+
+export default styles
