3c8071129740e9a17a1d0f675601c29f0afe6f4e
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-04-13 10:51:17 -0300

    feat: :seedling: Custom Title

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, Text } from "react-native";
+import styles from "./style";
 
 export default function Title() {
     return(
-        <View>
-            <Text>ONEBITHEALTH</Text>
+        <View style={styles.boxTitle}>
+            <Text style={styles.textTitle}>ONEBITHEALTH</Text>
         </View>
     );
 }
